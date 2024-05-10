882b5b3558955d9fab535dc167439adb5e20871e
Raoul1808 <raoulthegeek@gmail.com>
2024-05-11 01:28:04 +0200

    it's 1:30am I need sleep help

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,11 @@
-use std::io::Write;
+use std::io::{Read, Write};
 
 use rfd::FileDialog;
-use srtb_integrate_speeds::*;
 
 fn integrate_speeds(key: &str) -> Result<(), String> {
     println!("Select a chart to integrate speeds to");
     let file = FileDialog::new()
-        .add_filter("Spin Rhythm Track Bunidle", &["srtb"])
+        .add_filter("Spin Rhythm Track Bundle", &["srtb"])
         .pick_file();
     let srtb_file = file.ok_or("Please select a srtb file")?;
 
@@ -17,7 +16,7 @@ fn integrate_speeds(key: &str) -> Result<(), String> {
     let speeds_file = file.ok_or("Please select a speeds file")?;
 
     println!("Beginning process");
-    integrate(&srtb_file, &speeds_file, key)
+    srtb_integration_program::integrate(&srtb_file, &speeds_file, key)
 }
 
 fn extract_speeds(key: &str) -> Result<(), String> {
@@ -27,7 +26,7 @@ fn extract_speeds(key: &str) -> Result<(), String> {
         .pick_file();
     let srtb_file = file.ok_or("Please select a srtb file")?;
 
-    extract(&srtb_file, key)
+    srtb_integration_program::extract(&srtb_file, key)
 }
 
 fn remove_speeds(key: &str) -> Result<(), String> {
@@ -37,7 +36,24 @@ fn remove_speeds(key: &str) -> Result<(), String> {
         .pick_file();
     let srtb_file = file.ok_or("Please select a srtb file")?;
 
-    remove(&srtb_file, key)
+    srtb_integration_program::remove(&srtb_file, key)
+}
+
+fn integrate_chroma(key: &str) -> Result<(), String> {
+    println!("Select a chart to integrate speeds to");
+    let file = FileDialog::new()
+        .add_filter("Spin Rhythm Track Bundle", &["srtb"])
+        .pick_file();
+    let srtb_file = file.ok_or("Please select a srtb file")?;
+
+    println!("Select a speeds file to integrate");
+    let file = FileDialog::new()
+        .add_filter("Speed Triggers File", &["speeds"])
+        .pick_file();
+    let chroma_file = file.ok_or("Please select a speeds file")?;
+
+    println!("Beginning process");
+    srtb_integration_program::integrate_chroma(&srtb_file, &chroma_file, key)
 }
 
 fn map_num_to_key<'a>(opt: i32) -> Option<&'a str> {
@@ -53,43 +69,94 @@ fn map_num_to_key<'a>(opt: i32) -> Option<&'a str> {
     }
 }
 
+fn chroma_map_num_to_key<'a>(opt: i32) -> Option<&'a str> {
+    match opt {
+        1 => Some("SpeenChroma_ChromaTriggers_EASY"),
+        2 => Some("SpeenChroma_ChromaTriggers_NORMAL"),
+        3 => Some("SpeenChroma_ChromaTriggers_HARD"),
+        4 => Some("SpeenChroma_ChromaTriggers_EXPERT"),
+        5 => Some("SpeenChroma_ChromaTriggers_XD"),
+        6 => Some("SpeenChroma_ChromaTriggers_REMIXD"),
+        7 => Some("SpeenChroma_ChromaTriggers"),
+        _ => None,
+    }
+}
+
 pub fn program_flow() -> Result<(), String> {
-    println!("Please select a mode");
-    println!("1. Integrate speeds into srtb");
-    println!("2. Extract speeds from srtb");
-    println!("3. Remove speeds from srtb");
-    println!("4. Exit");
+    println!("Please select which kind of triggers you would like to integrate");
+    println!("1. Speed Triggers (Dynamic Track Speed)");
+    println!("2. Chroma Triggers (Speen Chroma 2)");
     print!("> ");
-    let mut buf = String::new();
     std::io::stdout().flush().expect("failed to flush stdout");
+    let mut buf = String::new();
     std::io::stdin()
         .read_line(&mut buf)
         .expect("failed to read from stdin");
-    let mode_opt: i32 = buf.trim_end().parse().expect("not a number");
+    let trigger_opt: i32 = buf.trim_end().parse().expect("not a number");
 
-    println!("Please select the target difficulty");
-    println!("1. Easy");
-    println!("2. Normal");
-    println!("3. Hard");
-    println!("4. Expert");
-    println!("5. XD");
-    println!("6. RemiXD");
-    println!("7. All (legacy)");
-    print!("> ");
-    let mut buf = String::new();
-    std::io::stdout().flush().expect("failed to flush stdout");
-    std::io::stdin()
-        .read_line(&mut buf)
-        .expect("failed to read from stdin");
-    let diff_opt: i32 = buf.trim_end().parse().expect("not a number");
+    match trigger_opt {
+        1 => {
+            println!("Please select a mode");
+            println!("1. Integrate speeds into srtb");
+            println!("2. Extract speeds from srtb");
+            println!("3. Remove speeds from srtb");
+            println!("4. Exit");
+            print!("> ");
+            let mut buf = String::new();
+            std::io::stdout().flush().expect("failed to flush stdout");
+            std::io::stdin()
+                .read_line(&mut buf)
+                .expect("failed to read from stdin");
+            let mode_opt: i32 = buf.trim_end().parse().expect("not a number");
 
-    let lookup_key = map_num_to_key(diff_opt).expect("invalid difficulty");
+            println!("Please select the target difficulty");
+            println!("1. Easy");
+            println!("2. Normal");
+            println!("3. Hard");
+            println!("4. Expert");
+            println!("5. XD");
+            println!("6. RemiXD");
+            println!("7. All (legacy)");
+            print!("> ");
+            let mut buf = String::new();
+            std::io::stdout().flush().expect("failed to flush stdout");
+            std::io::stdin()
+                .read_line(&mut buf)
+                .expect("failed to read from stdin");
+            let diff_opt: i32 = buf.trim_end().parse().expect("not a number");
 
-    match mode_opt {
-        1 => integrate_speeds(lookup_key),
-        2 => extract_speeds(lookup_key),
-        3 => remove_speeds(lookup_key),
-        4 => Ok(()),
-        _ => Err("invalid mode".into()),
+            let lookup_key = map_num_to_key(diff_opt).expect("invalid difficulty");
+
+            match mode_opt {
+                1 => integrate_speeds(lookup_key),
+                2 => extract_speeds(lookup_key),
+                3 => remove_speeds(lookup_key),
+                4 => Ok(()),
+                _ => Err("invalid mode".into()),
+            }
+        }
+        2 => {
+            println!("Please select the target difficulty");
+            println!("1. Easy");
+            println!("2. Normal");
+            println!("3. Hard");
+            println!("4. Expert");
+            println!("5. XD");
+            println!("6. RemiXD");
+            println!("7. All (legacy)");
+            print!("> ");
+
+            let mut buf = String::new();
+            std::io::stdout().flush().expect("failed to flush stdout");
+            std::io::stdin()
+                .read_line(&mut buf)
+                .expect("failed to read from stdin");
+            let diff_opt: i32 = buf.trim_end().parse().expect("not a number");
+
+            let lookup_key = chroma_map_num_to_key(diff_opt).expect("invalid difficulty");
+
+            integrate_chroma(lookup_key)
+        }
+        _ => Err("Invalid mode".into()),
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
 use std::{fmt::Write, fs, path::Path};
 
 use serde::{Deserialize, Serialize};
@@ -48,6 +50,158 @@ pub struct SpeedTrigger {
     pub time: f32,
     pub speed_multiplier: f32,
     pub interpolate_to_next_trigger: bool,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct ChromaTriggersData {
+    pub note_a: Vec<ChromaTrigger>,
+    pub note_b: Vec<ChromaTrigger>,
+    pub beat: Vec<ChromaTrigger>,
+    pub spin_left: Vec<ChromaTrigger>,
+    pub spin_right: Vec<ChromaTrigger>,
+    pub scratch: Vec<ChromaTrigger>,
+    pub ancillary: Vec<ChromaTrigger>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct ChromaTrigger {
+    pub time: f32,
+    pub duration: f32,
+    pub start_color: HslColor,
+    pub end_color: HslColor,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone, Copy)]
+#[serde(rename_all = "PascalCase")]
+pub struct HslColor {
+    pub h: f32,
+    pub s: f32,
+    pub l: f32,
+}
+
+impl HslColor {
+    pub fn from_hex_rgb(hex: &str) -> Result<HslColor, String> {
+        let mut hex = hex.to_string();
+        if hex.starts_with("#") {
+            hex = hex.replace("#", "");
+        }
+        if hex.len() != 6 {
+            return Err(format!(
+                "Invalid hex color code: expected 6 characters (excluding #), found {}",
+                hex.len()
+            ));
+        }
+
+        let r = u8::from_str_radix(&hex[0..2], 16).map_err(|_| "Invalid hex code".to_string())?;
+        let g = u8::from_str_radix(&hex[2..4], 16).map_err(|_| "Invalid hex code".to_string())?;
+        let b = u8::from_str_radix(&hex[4..6], 16).map_err(|_| "Invalid hex code".to_string())?;
+
+        let r = r as f64 / 255.;
+        let g = g as f64 / 255.;
+        let b = b as f64 / 255.;
+
+        let min = f64::min(r, f64::min(g, b));
+        let max = f64::max(r, f64::max(g, b));
+        let diff = max - min;
+        let l = (max + min) / 2.;
+
+        if diff == 0. {
+            return Ok(HslColor {
+                h: 0.,
+                s: 0.,
+                l: l as f32,
+            });
+        }
+
+        let s = if l < 0.5 {
+            diff / (max + min)
+        } else {
+            diff / (2. - max - min)
+        };
+        let r = (((max - r) / 6.) + (max / 2.)) / diff;
+        let g = (((max - g) / 6.) + (max / 2.)) / diff;
+        let b = (((max - b) / 6.) + (max / 2.)) / diff;
+
+        let h = if r == max {
+            b - g
+        } else if g == max {
+            (1. / 3.) + r - b
+        } else if b == max {
+            (2. / 3.) + g - r
+        } else {
+            0.
+        };
+
+        let h = if h < 0. {
+            h + 1.
+        } else if h > 1. {
+            h - 1.
+        } else {
+            h
+        };
+
+        Ok(HslColor {
+            h: h as f32,
+            s: s as f32,
+            l: l as f32,
+        })
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+pub enum ChromaNoteType {
+    NoteA,
+    NoteB,
+    Beat,
+    SpinLeft,
+    SpinRight,
+    Scratch,
+    Ancillary,
+}
+
+impl ChromaNoteType {
+    pub const ALL_NOTES: [ChromaNoteType; 7] = [
+        ChromaNoteType::NoteA,
+        ChromaNoteType::NoteB,
+        ChromaNoteType::Beat,
+        ChromaNoteType::SpinLeft,
+        ChromaNoteType::SpinRight,
+        ChromaNoteType::Scratch,
+        ChromaNoteType::Ancillary,
+    ];
+
+    pub fn from_str(note: &str) -> Result<ChromaNoteType, String> {
+        use ChromaNoteType::*;
+        let note = match note.to_lowercase().as_str() {
+            "notea" => NoteA,
+            "noteb" => NoteB,
+            "beat" => Beat,
+            "spinleft" | "leftspin" => SpinLeft,
+            "spinright" | "rightspin" => SpinRight,
+            "scratch" => Scratch,
+            "ancillary" | "highlights" => Ancillary,
+            _ => return Err(format!("Invalid note type: {}", note)),
+        };
+        Ok(note)
+    }
+}
+
+impl Display for ChromaNoteType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        use ChromaNoteType::*;
+        let str = match self {
+            NoteA => "Note A",
+            NoteB => "Note B",
+            Beat => "Beat",
+            SpinLeft => "Left Spin",
+            SpinRight => "Right Spin",
+            Scratch => "Scratch",
+            Ancillary => "Highlights",
+        };
+        write!(f, "{}", str)
+    }
 }
 
 pub fn speeds_to_json(content: &str) -> Result<SpeedTriggersData, String> {
@@ -129,6 +283,266 @@ pub fn json_to_speeds(speeds: &SpeedTriggersData) -> String {
     })
 }
 
+fn get_color(
+    color_map: &HashMap<ChromaNoteType, HslColor>,
+    note_type: ChromaNoteType,
+    color_str: &str,
+) -> Result<HslColor, String> {
+    let color_str = color_str.to_lowercase();
+    if color_str == "default" {
+        return color_map
+            .get(&note_type)
+            .map(|col| *col)
+            .ok_or(format!("no default color for note type {}", note_type));
+    }
+    if color_str.starts_with("default") {
+        let note_type = ChromaNoteType::from_str(&color_str[7..])?;
+        return color_map
+            .get(&note_type)
+            .map(|col| *col)
+            .ok_or(format!("no default color for note type {}", note_type));
+    }
+    let col = HslColor::from_hex_rgb(&color_str)?;
+    Ok(col)
+}
+
+pub fn chroma_to_json(content: &str) -> Result<ChromaTriggersData, String> {
+    let mut default_colors = HashMap::new();
+    let mut chroma_data = HashMap::new();
+    for note_type in ChromaNoteType::ALL_NOTES {
+        chroma_data.insert(note_type, vec![]);
+    }
+
+    for line in content.lines().enumerate() {
+        println!("Parsing line {:?}", line);
+        let (line_number, line) = line;
+        let line = line.trim().to_lowercase();
+        if line.is_empty() || line.starts_with("#") {
+            continue;
+        }
+        let line: Vec<_> = line.split_whitespace().collect();
+        if line.is_empty() || line[0].is_empty() {
+            continue;
+        }
+        let verb = line[0];
+        match verb {
+            "start" => {
+                if line.len() < 3 {
+                    return Err(format!(
+                        "Line {}: not enough arguments for trigger type `Start`",
+                        line_number
+                    ));
+                }
+                let note_type = ChromaNoteType::from_str(line[1])
+                    .map_err(|e| format!("Line {}: {}", line_number, e))?;
+                let color = HslColor::from_hex_rgb(line[2])
+                    .map_err(|e| format!("Line {}: {}", line_number, e))?;
+                chroma_data
+                    .get_mut(&note_type)
+                    .unwrap()
+                    .push(ChromaTrigger {
+                        time: 0.,
+                        duration: 0.,
+                        start_color: color,
+                        end_color: color,
+                    });
+                default_colors.insert(note_type, color);
+            }
+            "instant" => {
+                if line.len() < 4 {
+                    return Err(format!(
+                        "Line {}: not enough arguments for trigger type `Instant`",
+                        line_number
+                    ));
+                }
+                let note_type = ChromaNoteType::from_str(line[1])
+                    .map_err(|e| format!("Line {}: {}", line_number, e))?;
+                let time: f32 = line[2]
+                    .parse()
+                    .map_err(|_| format!("Line {}: invalid trigger time", line_number))?;
+                let color = get_color(&default_colors, note_type, line[3])
+                    .map_err(|e| format!("Line {}: {}", line_number, e))?;
+                chroma_data
+                    .get_mut(&note_type)
+                    .unwrap()
+                    .push(ChromaTrigger {
+                        time,
+                        duration: 0.,
+                        start_color: color,
+                        end_color: color,
+                    });
+            }
+            "swap" => {
+                if line.len() < 2 {
+                    return Err(format!(
+                        "Line {}: not enough arguments for trigger type `Swap`",
+                        line_number
+                    ));
+                }
+                match line[1] {
+                    "instant" => {
+                        if line.len() < 5 {
+                            return Err(format!(
+                                "Line {}: not enough arguments for trigger type `Swap Instant`",
+                                line_number
+                            ));
+                        }
+                        let time: f32 = line[2]
+                            .parse()
+                            .map_err(|_| format!("Line {}: invalid trigger time", line_number))?;
+                        let first_note_type = ChromaNoteType::from_str(line[3])
+                            .map_err(|e| format!("Line {}: {}", line_number, e))?;
+                        let second_note_type = ChromaNoteType::from_str(line[4])
+                            .map_err(|e| format!("Line {}: {}", line_number, e))?;
+                        let (first_col, second_col) = {
+                            let first_last_trigger =
+                                chroma_data.get(&first_note_type).unwrap().last().ok_or(
+                                    format!(
+                                        "Line {}: no trigger for {}",
+                                        line_number, first_note_type
+                                    ),
+                                )?;
+                            let second_last_trigger =
+                                chroma_data.get(&second_note_type).unwrap().last().ok_or(
+                                    format!(
+                                        "Line {}: no trigger for {}",
+                                        line_number, second_note_type
+                                    ),
+                                )?;
+                            (first_last_trigger.end_color, second_last_trigger.end_color)
+                        };
+                        chroma_data
+                            .get_mut(&first_note_type)
+                            .unwrap()
+                            .push(ChromaTrigger {
+                                time,
+                                duration: 0.,
+                                start_color: second_col,
+                                end_color: second_col,
+                            });
+                        chroma_data
+                            .get_mut(&second_note_type)
+                            .unwrap()
+                            .push(ChromaTrigger {
+                                time,
+                                duration: 0.,
+                                start_color: first_col,
+                                end_color: first_col,
+                            });
+                    }
+                    "flash" => {
+                        if line.len() < 7 {
+                            return Err(format!(
+                                "Line {}: not enough arguments for trigger type `Swap Flash`",
+                                line_number
+                            ));
+                        }
+                        let start_time: f32 = line[2]
+                            .parse()
+                            .map_err(|_| format!("Line {}: invalid trigger time", line_number))?;
+                        let end_time: f32 = line[3]
+                            .parse()
+                            .map_err(|_| format!("Line {}: invalid trigger time", line_number))?;
+                        let first_note_type = ChromaNoteType::from_str(line[4])
+                            .map_err(|e| format!("Line {}: {}", line_number, e))?;
+                        let second_note_type = ChromaNoteType::from_str(line[5])
+                            .map_err(|e| format!("Line {}: {}", line_number, e))?;
+                        let flash_col = HslColor::from_hex_rgb(line[6])
+                            .map_err(|e| format!("Line {}: {}", line_number, e))?;
+                        let (first_col, second_col) = {
+                            let first_last_trigger =
+                                chroma_data.get(&first_note_type).unwrap().last().ok_or(
+                                    format!(
+                                        "Line {}: no trigger for {}",
+                                        line_number, first_note_type
+                                    ),
+                                )?;
+                            let second_last_trigger =
+                                chroma_data.get(&second_note_type).unwrap().last().ok_or(
+                                    format!(
+                                        "Line {}: no trigger for {}",
+                                        line_number, second_note_type
+                                    ),
+                                )?;
+                            (first_last_trigger.end_color, second_last_trigger.end_color)
+                        };
+                        chroma_data
+                            .get_mut(&first_note_type)
+                            .unwrap()
+                            .push(ChromaTrigger {
+                                time: start_time,
+                                duration: end_time - start_time,
+                                start_color: flash_col,
+                                end_color: second_col,
+                            });
+                        chroma_data
+                            .get_mut(&second_note_type)
+                            .unwrap()
+                            .push(ChromaTrigger {
+                                time: start_time,
+                                duration: end_time - start_time,
+                                start_color: flash_col,
+                                end_color: first_col,
+                            });
+                    }
+                    _ => {
+                        return Err(format!(
+                            "Line {}: unknown `Swap` trigger subtype `{}`",
+                            line_number, line[1]
+                        ))
+                    }
+                }
+            }
+            _ => {
+                if line.len() < 5 {
+                    return Err(format!(
+                        "Line {}: not enough arguments for chroma trigger",
+                        line_number
+                    ));
+                }
+                let note_type = ChromaNoteType::from_str(line[0])
+                    .map_err(|e| format!("Line {}: {}", line_number, e))?;
+                let start_time: f32 = line[1]
+                    .parse()
+                    .map_err(|_| format!("Line {}: invalid trigger time", line_number))?;
+                let end_time: f32 = line[2]
+                    .parse()
+                    .map_err(|_| format!("Line {}: invalid trigger time", line_number))?;
+                let start_color = get_color(&default_colors, note_type, line[3])
+                    .map_err(|e| format!("Line {}: {}", line_number, e))?;
+                let end_color = get_color(&default_colors, note_type, line[4])
+                    .map_err(|e| format!("Line {}: {}", line_number, e))?;
+                chroma_data
+                    .get_mut(&note_type)
+                    .unwrap()
+                    .push(ChromaTrigger {
+                        time: start_time,
+                        duration: end_time - start_time,
+                        start_color,
+                        end_color,
+                    });
+            }
+        }
+    }
+
+    for (_, trigger_data) in chroma_data.iter_mut() {
+        trigger_data.sort_by(|a, b| a.time.total_cmp(&b.time));
+    }
+
+    {
+        use ChromaNoteType::*;
+        return Ok(ChromaTriggersData {
+            note_a: chroma_data.remove(&NoteA).unwrap(),
+            note_b: chroma_data.remove(&NoteB).unwrap(),
+            beat: chroma_data.remove(&Beat).unwrap(),
+            spin_left: chroma_data.remove(&SpinLeft).unwrap(),
+            spin_right: chroma_data.remove(&SpinRight).unwrap(),
+            scratch: chroma_data.remove(&Scratch).unwrap(),
+            ancillary: chroma_data.remove(&Ancillary).unwrap(),
+        });
+    }
+}
+
 pub fn integrate(srtb: &Path, speeds: &Path, diff_key: &str) -> Result<(), String> {
     println!("Reading file contents");
     let chart_contents = fs::read_to_string(srtb).map_err(|e| e.to_string())?;
@@ -155,6 +569,46 @@ pub fn integrate(srtb: &Path, speeds: &Path, diff_key: &str) -> Result<(), Strin
             .push(LargeStringValue {
                 key: diff_key.to_string(),
                 val: speeds_json.clone(),
+            });
+    }
+    let chart = serde_json::to_string(&chart).map_err(|e| e.to_string())?;
+
+    println!("Integration complete! Please select where you would like to save your file");
+    let file = rfd::FileDialog::new()
+        .add_filter("Spin Rhythm Track Bundle", &["srtb"])
+        .save_file();
+    let dest_file = file.ok_or("no destination file selected")?;
+    fs::write(dest_file, chart).map_err(|e| e.to_string())?;
+    println!("All done!");
+    Ok(())
+}
+
+pub fn integrate_chroma(srtb: &Path, chroma: &Path, diff_key: &str) -> Result<(), String> {
+    println!("Reading file contents");
+    let chart_contents = fs::read_to_string(srtb).map_err(|e| e.to_string())?;
+    let speeds_contents = fs::read_to_string(chroma).map_err(|e| e.to_string())?;
+
+    println!("Converting speeds");
+    let chroma = chroma_to_json(&speeds_contents)?;
+    let chroma_json = serde_json::to_string(&chroma).map_err(|e| e.to_string())?;
+
+    println!("Integrating to srtb");
+    let mut chart: RawSrtbFile =
+        serde_json::from_str(&chart_contents).map_err(|e| e.to_string())?;
+    if let Some(value) = chart
+        .large_string_values_container
+        .values
+        .iter_mut()
+        .find(|v| v.key == diff_key)
+    {
+        value.val.clone_from(&chroma_json);
+    } else {
+        chart
+            .large_string_values_container
+            .values
+            .push(LargeStringValue {
+                key: diff_key.to_string(),
+                val: chroma_json.clone(),
             });
     }
     let chart = serde_json::to_string(&chart).map_err(|e| e.to_string())?;
