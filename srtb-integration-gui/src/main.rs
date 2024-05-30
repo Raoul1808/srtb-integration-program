8f2e177b32353f9e3168b2e94064a879d18b2855
Raoul1808 <raoulthegeek@gmail.com>
2024-05-30 15:04:45 +0200

    Create GUI with iced

--- srtb-integration-gui/src/main.rs
+++ srtb-integration-gui/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use iced::{widget::text, Sandbox, Settings, Size};
+
+fn main() -> iced::Result {
+    let mut settings = Settings::default();
+    settings.window.size = Size::new(360., 450.);
+    App::run(settings)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Message {}
+
+struct App;
+
+impl Sandbox for App {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        "SRTB Integration Program".into()
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {}
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        text("Hello, world!").into()
+    }
 }
