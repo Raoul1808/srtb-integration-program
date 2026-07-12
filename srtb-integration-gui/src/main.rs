e9d09b076ba5807c5e4d13d890770e9165d9f7ba
Raoul1808 <raoulthegeek@gmail.com>
2026-07-12 19:53:55 +0200

    that shit won't deploy anymore yippee

--- srtb-integration-gui/src/main.rs
+++ srtb-integration-gui/src/main.rs
@@ -1,15 +1,9 @@
 #[cfg(not(target_arch = "wasm32"))]
 mod desktop;
 
-#[cfg(target_arch = "wasm32")]
-mod web;
-
 fn main() -> iced::Result {
     #[cfg(not(target_arch = "wasm32"))]
     desktop::program()?;
-
-    #[cfg(target_arch = "wasm32")]
-    web::program()?;
 
     Ok(())
 }
